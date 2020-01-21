d75fbfb2be6d8ab0e18f2562efe2db40e258b510
nildemarco <nildemarco@gmail.com>
2020-01-20 21:30:38 -0300

    Avances de la primera clase

---
+++ main.js
@@ -0,0 +1,84 @@
+const gatos = [
+    {
+      name: "Rodolfo",
+      shortDesc: "Tiene 4 años, le gusta perseguir mariposas, se lleva bien con niños y con otros gatos.",
+      longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+      img: "http://www.placekitten.com/400"
+    },
+  
+    {
+      name: "Muzzarella",
+      shortDesc: "Muy dulce y mimosa. Tiene seis dedos en una pata que asegura le dan superpoderes.",
+      longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+      img: "http://www.placekitten.com/500"
+    },
+  
+    {
+      name: "Artilugia",
+      shortDesc: "Muy activa y juguetona. Se lleva bien con perros. Ideal para casa con jardin amplio",
+      longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+      img: "http://www.placekitten.com/600"
+    },
+  
+    {
+      name: "Wosito",
+      shortDesc: "Vivio toda su vida en la calle y todavia se asombra de cosas como estufas y escaleras.",
+      longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+      img: "http://www.placekitten.com/700"
+    },
+  
+    {
+      name: "Calamardo",
+      shortDesc: "Dicen que de noche, cuando nadie lo puede escuchar, invoca a Cthulu. Muy mimoso.",
+      longDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit libero dolorum aliquam error expedita distinctio porro ducimus ex repellendus laboriosam. Sequi, doloribus autem? Unde commodi assumenda consequatur ratione numquam distinctio nihil blanditiis quae debitis sed eligendi modi architecto omnis aspernatur officia molestiae, vero nemo a quibusdam? Voluptatum eveniet blanditiis impedit.",
+      img: "http://www.placekitten.com/700"
+    },
+    
+  ]
+
+  let acumuladoraCards = "";
+
+
+for (let i = 0; i < gatos.length; i++) {
+    acumuladoraCards += `
+    <div>
+    <img src=${gatos[i].img} alt=${gatos[i].name}>
+    <h3>${gatos[i].name}</h3>
+    <p>${gatos[i].shortDesc}</p>
+    <button class="boton-vermas">Ver mas</button>
+    </div>
+    `;
+  
+}
+console.log(acumuladoraCards)
+const tarjetaDeGatitos = document.getElementById("cards");
+
+tarjetaDeGatitos.innerHTML = acumuladoraCards;
+console.log(acumuladoraCards)
+
+const modal = document.getElementById("myModal");
+
+const botonAbrirModal = document.getElementsByTagName("img");
+const modalcontenido = document.querySelector(".modalcontent")
+
+for (let i = 0; i < botonAbrirModal.length; i++) {
+    botonAbrirModal[i].onclick = () => {
+        console.log(`Cualquier cosa`)
+        modal.classList.remove("nomostrar")
+        
+        modalcontenido.innerHTML = `
+        <img src=${gatos.img} alt=${gatos[i].name}>
+        <div>
+        <h3>${gatos[i].name}<h3>
+        <p>${gatos[i].longDesc}</p></div>
+        `
+    }
+    
+}
+const botonCerrarModal = document.getElementById("cerrarModal")
+
+
+botonCerrarModal.onclick = () => {
+    console.log(`Aprete boton`)
+    modal.classList.add("nomostrar")
+}
